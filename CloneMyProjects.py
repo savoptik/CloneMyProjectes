69f2ac88d5963de39a045897ef58870d2a725712
ArtemSemenov <sav70792@gmail.com>
2020-03-22 15:57:16 +0300

    script ccheced existing dirs

--- CloneMyProjects.py
+++ CloneMyProjects.py
@@ -1,6 +1,12 @@
 import os
 print(os.uname())
-print("привет мир")
+
 with open("Repolinc") as file:
     arry = [row.strip() for  row in file]
-    print(arry)
+for row in  arry:
+    dirName = row[row.rfind("/"):-4]
+    print(dirName)
+    if os.path.exists("/users/artemsemenov/projects/myprojects/" + dirName):
+        print("папка найдена")
+    else:
+        print("папка не найдена")
