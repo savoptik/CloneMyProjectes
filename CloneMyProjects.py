dae4cf091a7d59e7cd53350934dd9b8b3139f0c2
ArtemSemenov <sav70792@gmail.com>
2020-03-22 16:03:55 +0300

    change abs path to work dir

--- CloneMyProjects.py
+++ CloneMyProjects.py
@@ -6,7 +6,7 @@ with open("Repolinc") as file:
 for row in  arry:
     dirName = row[row.rfind("/"):-4]
     print(dirName)
-    if os.path.exists("/users/artemsemenov/projects/myprojects/" + dirName):
+    if os.path.exists(os.getcwd() + dirName):
         print("папка найдена")
     else:
         print("папка не найдена")
